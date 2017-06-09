6f6012f8f080ff6774e8160cab456248806f2c4e
CamStu <cameronstuartcrs@gmail.com>
2017-06-08 17:03:24 -0700

    Rudamentry matrix multiplication start

--- CUDA_matrixMult/CUDA_matrixMult/kernel.cu
+++ CUDA_matrixMult/CUDA_matrixMult/kernel.cu
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <math.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include "device_launch_parameters.h"
+
 // Kernel function to add the elements of two arrays
 __global__
 void add(int n, float *x, float *y)
@@ -8,36 +12,55 @@ void add(int n, float *x, float *y)
 		y[i] = x[i] + y[i];
 }
 
+//Multiplies a,b stores result in c
+__global__
+void MatrixMult(float *a, float *b, float *c) {
+	int row = blockIdx.x*blockDim.x + threadIdx.x;
+	int col = blockIdx.y*blockDim.y + threadIdx.y;
+	float val = 0;
+
+	if (row > 10000 || col > 10000)
+		return;
+
+	for (int i = 0; i < 10000; i++) {
+		val += a[row * 10000 + i] * b[i * 10000 + col];
+	}
+	c[row * 10000 + col] = val;
+}
+
+
 int main(void)
 {
-	int N = 1 << 20;	//Million elements
-	float *x, *y;
+//	int N = 1 << 20;	//Million elements
+	int width = 10000, height = 10000; //10,000x10,000
+
+	float *x, *y, *z;
 
 	// Allocate Unified Memory – accessible from CPU or GPU
-	cudaMallocManaged(&x, N * sizeof(float));
-	cudaMallocManaged(&y, N * sizeof(float));
+	// While 1D arrays, these will be accessed by converting 2D index space to linear space
+	cudaMallocManaged(&x, width*height * sizeof(float));	
+	cudaMallocManaged(&y, width*height * sizeof(float));
+	cudaMallocManaged(&z, width*height * sizeof(float));
 
-	// initialize x and y arrays on the host
-	for (int i = 0; i < N; i++) {
-		x[i] = 1.0f;
-		y[i] = 2.0f;
-	}
-
-	// Run kernel on 1M elements on the GPU
-	add << <1, 1 >> >(N, x, y);
-
+	//Initialize arrays on host
+	for(int i=0; i < width; i++)
+		for (int j = 0; j < height; j++) {
+			x[i*width + j] = 1.0f;
+			y[i*width + j] = 2.0f;
+		}
+	//A 10,000x10,000 Product matrix could be computer with one thread per entry with 100x100 blocks with 100x100 threads each
+	dim3 blockSize(100, 100);
+	dim3 gridSize(100, 100);
+	MatrixMult <<< gridSize, blockSize >>> (x, y, z);	//Block and thread dimensions chosen to be within hardware constraints
 	// Wait for GPU to finish before accessing on host
 	cudaDeviceSynchronize();
-
-	// Check for errors (all values should be 3.0f)
-	float maxError = 0.0f;
-	for (int i = 0; i < N; i++)
-		maxError = fmax(maxError, fabs(y[i] - 3.0f));
-	std::cout << "Max error: " << maxError << std::endl;
+	printf("Operation complete\n");
+	
 
 	// Free memory
 	cudaFree(x);
 	cudaFree(y);
+	cudaFree(z);
 
 	return 0;
 }
